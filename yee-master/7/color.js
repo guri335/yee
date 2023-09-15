ddee2e2c94be2f49718a1ad8cf3a291b92bb2f41
guri335 <ue218045.gurwinder.it@gmail.com>
2023-09-15 11:06:47 +0100

    g

--- yee-master/7/color.js
+++
@@ -1,5 +0,0 @@
-var colors=["red","blue","green","purple","orange","black","maroon","lime"]
-function changecolor(){
-    var num=Math.floor(Math.random()*(colors.length))
-    document.getElementById("target").style.backgroundColor =colors[num];
-}
